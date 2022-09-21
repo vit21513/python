4f1ed03c9934a091c8a4fac29de07241e39bb355
«vitaly» <vit21513@yandex.ru>
2022-09-21 23:10:53 +0300

    b

--- 2.py
+++ 2.py
@@ -1,16 +1,18 @@
 
-def Days(num):
-    week= ['Понедельник', 'Вторник','Среда','Четверг','Пятница','Субота','Воскресенье']
-    if (num < 1 or num > 7):
-        print("Вы ввели некоректное число")
-    elif num >= 6:
-        print(f"{week[num-1]} это выходной день")
+import math
+
+
+num1=int(input("Введите 1 число "))
+
+num2= int(input("Введите 2 число "))
+
+
+def sqr_num(a,b):
+    res= a*b
+
+    if a == b*b:
+        print(f" число,{a}, является квадратом, {b}") 
     else:
-        print(f"{week[num-1]} это рабочий день")
+        print(f" число,{a}, не является квадратом, {b}")
 
-
-
-days =int(input("Введите день недели "))
-
-Days(days)
-
+sqr_num(num1,num2)
